d8639cdeeceac8d3e89991fd2358eac6d16181db
“Brian <brianshanley242@gmail.com>
2023-04-26 20:53:20 -0400

    Continued merge conflict fix

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 import Home from "./Home";
 import Login from "./Login";
@@ -6,16 +5,6 @@ import Cart from "./Cart";
 import { useSelector, useDispatch } from "react-redux";
 import { loginWithToken, fetchCart } from "../store";
 import { Link, Routes, Route } from "react-router-dom";
-=======
-import React, { useEffect } from 'react';
-import Home from './Home';
-import Login from './Login';
-import Cart from './Cart';
-import Products from './Products';
-import { useSelector, useDispatch } from 'react-redux';
-import { loginWithToken, fetchCart, fetchProducts } from '../store';
-import { Link, Routes, Route } from 'react-router-dom';
->>>>>>> main
 
 const App = () => {
   const { auth } = useSelector((state) => state);
@@ -34,7 +23,6 @@ const App = () => {
   return (
     <div>
       <h1>Acme Shopping</h1>
-<<<<<<< HEAD
       {auth.id ? <Home /> : <Login />}
       {!!auth.id && (
         <div>
@@ -47,27 +35,6 @@ const App = () => {
           </Routes>
         </div>
       )}
-=======
-      {
-        auth.id ? 
-        <Home /> : <Login />
-      }
-      {
-        !!auth.id  && (
-          <div>
-            <nav>
-              <Link to='/'>Home</Link>
-              <Link to='/products'>Products</Link>
-              <Link to='/cart'>Cart</Link>
-            </nav>
-            <Routes>
-              <Route path='/cart' element={ <Cart /> } />
-              <Route path='/products' element={ <Products /> } />
-            </Routes>
-          </div>
-        )
-      }
->>>>>>> main
     </div>
   );
 };
